7a11862296ec06bb2d2daee5b368cc1d0b1cacae
Patrick Preilowski <Patrickpreilowski@gmail.com>
2026-04-05 22:20:02 +0200

    Change AI bot header icon: brain→microchip

--- javascripts/discourse/api-initializers/gradient-icons.js
+++ javascripts/discourse/api-initializers/gradient-icons.js
@@ -100,7 +100,7 @@ export default apiInitializer("0.8", (api) => {
       if (this.clickShouldRouteOutOfConversations) {
         return "arrow-left";
       }
-      return "brain";
+      return "microchip";
     },
   });
 });
